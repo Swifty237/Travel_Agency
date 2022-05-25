6b2eb31ac85c1bbdd44f4a40469da0429e6064c4
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-26 01:52:47 +0200

    build all screen

--- components/Circuits.js
+++ components/Circuits.js
@@ -1,14 +1,60 @@
-import { Text, View, ImageBackground } from "react-native";
+import { Text, View, ImageBackground, StyleSheet, ScrollView } from "react-native";
+import { Header, SearchBar } from "@rneui/themed";
 
 const backImg = require("../assets/backimg_circuits.png")
 
 
 export default function Circuits() {
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ImageBackground>
-        <Text>Ok Circuits</Text>
+    <View style={styles.container}>
+      <ImageBackground style={styles.imgBackgrd} source={backImg}>
+        <Header
+          centerComponent={{ text: "CIRCUITS", style: styles.heading }}
+          containerStyle={styles.headContainer} backgroundColor="transparent" />
+        <SearchBar placeholder="ex. John DOE" containerStyle={styles.searchBar} />
+        <ScrollView>
+          <Text> Ok Services </Text>
+        </ScrollView>
       </ImageBackground>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  headContainer: {
+    backgroundColor: "black",
+    opacity: .5,
+    width: "95%",
+    borderRadius: 10,
+    marginTop: 10
+  },
+
+  imgBackgrd: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center"
+  },
+
+  heading: {
+    height: 100,
+    marginTop: 25,
+    alignItems: "center",
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "white"
+  },
+
+  searchBar: {
+    width: "95%",
+    opacity: .5,
+    marginTop: 10,
+    borderRadius: 10
+  }
+})
--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,60 @@
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground, StyleSheet, ScrollView } from "react-native";
+import { Header, SearchBar } from "@rneui/themed";
 
 const backImg = require("../assets/backimg_contact.png")
 
 export default function Contact() {
+
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Ok Contact</Text>
+        <View style={styles.container}>
+            <ImageBackground style={styles.imgBackgrd} source={backImg}>
+                <Header
+                    centerComponent={{ text: "CONTACT", style: styles.heading }}
+                    containerStyle={styles.headContainer} backgroundColor="transparent" />
+                <SearchBar placeholder="ex. John DOE" containerStyle={styles.searchBar} />
+                <ScrollView>
+                    <Text> Ok Services </Text>
+                </ScrollView>
+            </ImageBackground>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    headContainer: {
+        backgroundColor: "black",
+        opacity: .5,
+        width: "95%",
+        borderRadius: 10,
+        marginTop: 10
+    },
+
+    imgBackgrd: {
+        flex: 1,
+        width: "100%",
+        alignItems: "center"
+    },
+
+    heading: {
+        height: 100,
+        marginTop: 25,
+        alignItems: "center",
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "white"
+    },
+
+    searchBar: {
+        width: "95%",
+        opacity: .5,
+        marginTop: 10,
+        borderRadius: 10
+    }
+})
+
